aa4b014ca3c45d7b07281a45042ae0d58442541d
shannonhilland <shannonhilland@gmail.com>
2024-10-15 15:18:54 -0600

    create page.js

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Page() {
       <li><Link href="./week-2" className="underline text-blue-900 hover:text-blue-600">Week 2</ Link></li>
       <li><Link href="./week-3" className="underline text-blue-900 hover:text-blue-600">Week 3</Link></li>
       <li><Link href="./week-4" className="underline text-blue-900 hover:text-blue-600">Week 4</Link></li>
+      <li><Link href="./week-5" className="underline text-blue-900 hover:text-blue-600">Week 5</Link></li>
     </div>
   );
 }
---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return(
+        <main className="m-4">
+            <Link href="/" className="m-2 underline text-blue-900 hover:text-blue-600 text-sm">Home</Link>
+            
+        </main>
+    );
+}
