00467c5290a0428aca407595e0c2063cd1ea8c62
shannonhilland <shannonhilland@gmail.com>
2024-10-15 22:46:23 -0600

    add name and category

---
+++ app/week-5/new-item.js
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState} from "react";
+import Select from "react-select";
+
+const options = [
+    { value: "produce", label: "🍇 Produce" },
+    { value: "dairy", label: "🥛 Dairy" },
+    { value: "bakery", label: "🥐 Bakery" },
+    { value: "meat", label: "🍖 Meat" },
+    { value: "frozen foods", label: "❄️ Frozen Foods" },
+    { value: "canned goods", label: "🥫 Canned Goods" },
+    { value: "beverages", label: "🥤 Beverages" },
+    { value: "snacks", label: "🍿 Snacks" },
+    { value: "household", label: "🧼 Household" },
+    { value: "miscellaneous", label: "🛒 Miscellaneous" }
+];
+
+export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState([{value: "produce", label: "🍇 Produce"}]);
+    
+    const increment = () => {
+        if(quantity < 20) {
+            setQuantity(quantity + 1);
+        }
+    };
+    const decrement = () => {
+        if(quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(name == "") {
+            alert("Please enter an item name!");
+            return;
+        }
+        let item = {name, quantity, category};
+        console.log(item);
+
+        alert(`Name: ${name} Quantity: ${quantity} Category: ${category.label}`);
+        setQuantity(1);
+        setName("");
+        setCategory([{value: "produce", label: "🍇 Produce"}]);
+    }
+ 
+  return (
+    <div className="w-10/12 max-w-96 h-16 m-2">
+        <form>
+        <div>
+            {/* Required is not working here */}
+            <input type="text" placeholder="Item Name"  onChange={(event) => setName(event.target.value)} className="w-full h-8 p-1 my-2 border border-black rounded" value={name}/>
+        </div>
+        <div className="flex my-2">
+            <div className="flex items-center justify-around border  border-black rounded">
+                <div className="flex-grow p-3">
+                    <p className="text-center text-2xl w-14">{quantity}</p>
+                </div>
+                <div className="flex justify-center w-16 p-3">
+                    <button onClick={decrement} disabled={quantity == 1} className="text-2xl px-4 py-1 bg-blue-300 hover:bg-blue-500 disabled:bg-gray-400 rounded">-</button>
+                </div>
+                <div className="flex justify-center w-16 p-3">
+                    <button onClick={increment} disabled={quantity == 20} className="text-2xl px-4 py-1 bg-blue-300 hover:bg-blue-500 disabled:bg-gray-400 rounded">+</button>
+                </div>  
+            </div>
+            <div className="flex-grow">
+                {/* default is undefined in alert if not selected? */}
+                <Select 
+                    options={options}
+                    onChange={setCategory}
+                    className="m-3"
+                    defaultValue={category}
+                    isSearchable /> 
+            </div>
+        </div>
+        <div>
+            <button onClick={handleSubmit} className="w-64 h-8 bg-blue-300 hover:bg-blue-500 rounded">Add Item</button>
+        </div>
+        </form>
+    </div>
+  );
+    
+}
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
+import NewItem from "./new-item";
+
 export default function Page() {
     return(
         <main className="m-4">
             <Link href="/" className="m-2 underline text-blue-900 hover:text-blue-600 text-sm">Home</Link>
-            
+            <NewItem />
         </main>
     );
 }
